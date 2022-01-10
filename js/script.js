71abd1b6f346403ebb02d7dc8b6280f5ec3b0fcf
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-10 15:24:48 +0100

    sistemare

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  const output = document.querySelector('.team-container');
+  
+  for (let i = 1; i < team.length; i++) {
+    const persona = team[i]
+    // output.innerHTML += `<br/><h2>${persona.name}</h2>`
+    
+    for (let k in persona) {
+      const card = `
+      <div class="team-card">
+            <div class="card-image">
+            <img
+            src="img/${persona.image}"
+            alt="Wayne Barnett"
+          />
+            </div>
+            <div class="card-text">
+              <h3>${persona.name}</h3>
+              <p>${persona.role}</p>
+            </div>
+          </div>
+      `
+      output.innerHTML += card
+    }
+  }
